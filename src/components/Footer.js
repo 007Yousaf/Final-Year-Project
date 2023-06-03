93efee6821a8269099a217e6551daf9c523e8682
Rao_Yousaf <109105226+007Yousaf@users.noreply.github.com>
2023-06-03 09:12:00 +0500

    myComit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="text-center text-white bg-dark p-3" style={{ position: "fixed", bottom: 0, left: 0, right: 0 }}>
-      © 2023 Zia Ullah & Team
+      © 2023 Zia Ullah & M Yousaf
     </footer>
   );
 };
